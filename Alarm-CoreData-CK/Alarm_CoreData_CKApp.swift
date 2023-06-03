b9b6eb79ddda4588921613b7d641dcd092c3593f
James Lea <james.q.lea@gmail.com>
2023-06-03 11:16:31 -0600

    first commit

---
+++ Alarm-CoreData-CK/Alarm_CoreData_CKApp.swift
@@ -0,0 +1,17 @@
+//
+//  Alarm_CoreData_CKApp.swift
+//  Alarm-CoreData-CK
+//
+//  Created by James Lea on 6/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Alarm_CoreData_CKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
