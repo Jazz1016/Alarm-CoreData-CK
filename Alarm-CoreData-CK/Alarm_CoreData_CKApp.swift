a9e6b3b2d6f2e6e929bc56769daf546243af3fd4
James Lea <james.q.lea@gmail.com>
2023-06-08 09:37:18 -0600

    got Notifications showing trying to make sure they're showing, also working on background fetch

--- Alarm-CoreData-CK/Alarm_CoreData_CKApp.swift
+++ Alarm-CoreData-CK/Alarm_CoreData_CKApp.swift
@@ -6,13 +6,63 @@
 //
 
 import SwiftUI
+import BackgroundTasks
 
 @main
 struct Alarm_CoreData_CKApp: App {
+    
+    private var delegate: NotificationDelegate = NotificationDelegate()
+    
+    init() {
+        let center = UNUserNotificationCenter.current()
+        center.delegate = delegate
+        center.requestAuthorization(options: [.alert, .sound, .badge]) { result, error in
+            if let error = error {
+                print(error)
+            }
+        }
+        
+        registerBackgroundFetch()
+    }
+    
     var body: some Scene {
         WindowGroup {
             AlarmListView()
                 .environment(\.managedObjectContext, AlarmContainer().persistentContainer.viewContext)
         }
+    }
+    
+    func registerBackgroundFetch() {
+        BGTaskScheduler.shared.register(forTaskWithIdentifier: "com.yourapp.backgroundfetch", using: nil) { task in
+            self.handleBackgroundFetch(task: task as! BGAppRefreshTask)
+        }
+    }
+    
+    // Handle the background fetch task
+    func handleBackgroundFetch(task: BGAppRefreshTask) {
+        task.expirationHandler = {
+            // Handle expiration if necessary
+        }
+        
+        performBackgroundFetch { result in
+            switch result {
+            case .newData:
+                task.setTaskCompleted(success: true)
+            case .noData:
+                task.setTaskCompleted(success: false)
+            case .failed:
+                task.setTaskCompleted(success: false)
+            @unknown default:
+                task.setTaskCompleted(success: false)
+            }
+        }
+    }
+    
+    // Perform the background fetch
+    func performBackgroundFetch(completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        // Perform your background tasks, such as syncing with a server or updating data
+        
+        // Call the completion handler with the appropriate result
+        completionHandler(.newData) // Or .noData or .failed
     }
 }
